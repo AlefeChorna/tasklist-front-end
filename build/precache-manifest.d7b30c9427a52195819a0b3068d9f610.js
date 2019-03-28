bcbb2dda06e8180469dbe9191c440e92e1f59c60
Aléfe Chorna <alefechorna99@gmail.com>
2019-03-28 01:40:54 -0300

    Ajustes no fonte

---
+++ build/precache-manifest.d7b30c9427a52195819a0b3068d9f610.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6979468c7abc33ac3415",
+    "url": "/static/js/main.a1090e5a.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "91ece91b412901937d5e",
+    "url": "/static/css/2.08a4d43e.chunk.css"
+  },
+  {
+    "revision": "91ece91b412901937d5e",
+    "url": "/static/js/2.8060a17d.chunk.js"
+  },
+  {
+    "revision": "55da8a43c11f3dd44486264ebb4cee0b",
+    "url": "/static/media/notasks.55da8a43.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "93da327664d74a3ab8a3ed7de76a6111",
+    "url": "/index.html"
+  }
+];
